c89a9d8d19bd315ef85dd46cd9bf16a8f43828ff
Tcomp <williamt7786@gmail.com>
2021-04-26 02:58:51 -0400

    This commit was to get the question to pop up after the start button is clicked

--- assets/script.js
+++ assets/script.js
@@ -3,36 +3,25 @@ var Start = document.getElementById("startbtn")
 var quiz = document.getElementById("Quiz");
 var questionsEl = document.getElementById("question");
 var Timer = document.getElementById("timer");
-var buttonA1 = document.getElementById("btn1")
-var buttonA1 = document.getElementById("btn2")
-var buttonA1 = document.getElementById("btn3")
-var buttonA1 = document.getElementById("btn4")
+var choiceA = document.getElementById("a")
+var choiceB = document.getElementById("b")
+var choiceC = document.getElementById("c")
+var choiceD = document.getElementById("d")
 var results = document.getElementById("Yourscore")
 var end = document.getElementById("End")
-
-// var fQI = quizQuestions.length;
-var CQI = 0;
-var TimeLeft = 60;
-var timer;
-
-
-
-
 
 
 
 //functions
 Start.addEventListener("click", StartQ)
 function StartQ(){
+QuizQ();
 Start.classList.add("hide")
 quiz.classList.remove("hide")
 console.log("This function is running")
-
 timerInterval = setInterval(function() {
-    
     TimeLeft--;
     Timer.textContent = "Time: " + TimeLeft;
-
     if(TimeLeft === 0) {
       clearInterval(timerInterval);
       showScore();
@@ -40,29 +29,66 @@ timerInterval = setInterval(function() {
   }, 1000);
 }
 
+function QuizQ() {
+var currentQuestion = questions[currentQuestionIndex];
+questionsEl.innerHTML =  "<p>" + currentQuestion.question + "</p>"
+choiceA.innerHTML = currentQuestion.texta;
+choiceB.innerHTML = currentQuestion.textb;
+choiceC.innerHTML = currentQuestion.textc;
+choiceD.innerHTML = currentQuestion.tectd;
+}
 
 
-
-
-
+function check(answer){
+    correct = questions[currentQuestionIndex].correct;
+    if (answer === correct ) {
+    score++
+    currentQuestionIndex++
+    QuizQ()
+    }else( answer !== correct)
+    TimeLeft--;
+    
+   
+        
+};
 
 //Questions
-    const questions = [{
+    var questions = [{
         question: "Hello1",
-        answers: [
-            {text: "hi", correct: true},
-            {text: "hey", Incorrect: false},
-            {text: "Bye", Incorrenct: false},
-            {text: "no", Incorrect: false},
-        ],
-        question: "Hello2",
-        answers: [
-            {text: "a", correct: true},
-            {text: "b", Incorrect: false},
-            {text: "c", Incorrenct: false},
-            {text: "d", Incorrect: false},
-        ]
-    },
-   
+        texta: "hi", 
+        textb: "hey", 
+        textc: "Bye",
+        textd: "no", 
+        correctAnswer:"btn3"},
+        
+        {question: "Hello2",
+        texta:"no",
+        textb:"yes",
+        textc:"maybe",
+        textd: "maybenot",
+        correctAnswer: "btn2"},
+
+        {question: "Hello3",
+        texta:"no",
+        textb:"yes",
+        textc:"maybe",
+        textd: "maybenot",
+        correctAnswer: "btn2"},
+
+        {question: "Hello4",
+        texta:"no",
+        textb:"yes",
+        textc:"maybe",
+        textd: "maybenot",
+        correctAnswer: "btn2"},
+    ];
+
+var fQI = questions.length;
+var currentQuestionIndex = 1;
+var TimeLeft = 60;
+var timer;
+ var correct;
+
+
+
     
-    ]
